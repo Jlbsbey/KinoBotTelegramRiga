36629ecfeacc7f132ebb53c7cecf4c470e8633ed
Jlbsbey <nikita.smorigo@gmail.com>
2023-11-14 19:57:56 +0200

    init

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
 
 public class Bot extends TelegramLongPollingBot {
+    ArrayList<Long> IDs = new ArrayList<>();
     @Override
     public String getBotUsername() {
         return "Kino Bot Riga";
@@ -16,6 +21,51 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update);
+        var msg = update.getMessage();
+        if(msg.isCommand()){
+            if(msg.getText().equals("/echoall")){
+                echoAll();
+            }else if(msg.getText().equals("/deleteevryone")){
+                deleteEveryone();
+            };
+        }
+
+    }
+
+    public void deleteEveryone(){
+
+    }
+
+    public void echoAll(){
+
+    }
+
+    public boolean ifExists(Long userID){
+        boolean appears= false;
+        for (Long id : IDs) {
+            if (userID.equals(id)) {
+                appears = true;
+                return true;
+            }
+        }
+        if(appears==false){
+            addUser(userID);
+        }
+        return false;
+    }
+
+    public void addUser(Long userID){
+        IDs.add(userID);
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
     }
 }
